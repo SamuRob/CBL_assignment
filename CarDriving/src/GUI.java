6557f28646a8d5101223c441197e4714b9cd8d87
FierceTiger55043 <chinkeylolface@gmail.com>
2024-10-08 16:39:15 +0300

    Update

---
+++ CarDriving/src/GUI.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GUI {
+    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+
+        JPanel pane = new JPanel();
+
+    }
+}
